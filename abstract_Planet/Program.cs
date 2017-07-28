e1b5d57c3a4f67bea07cb34f50d0ad304a535c95
Lovejot Singh <lovejotsangha122@gmail.com>
2017-07-28 13:07:08 -0400

    update planet

--- abstract_Planet/Program.cs
+++ abstract_Planet/Program.cs
@@ -15,10 +15,12 @@ namespace abstract_Planet
             TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Earth", 12742, 5.9722, true);
             GiantPlanet giantPlanet1 = new GiantPlanet("Venus", 12104, 4.867, "GAS");
             GiantPlanet giantPlanet2 = new GiantPlanet("Mars", 6779, 6.39, "GAS");
+            GiantPlanet giantPlanet3 = new GiantPlanet("Jupiter", 139822, 317.8, "GAS");
             giantPlanet.ToString();
             giantPlanet1.ToString();
             terrestrialPlanet.ToString();
             giantPlanet2.ToString();
+            giantPlanet3.ToString();
             Program.WAITFORANYKEY();
         }
         static void WAITFORANYKEY()
