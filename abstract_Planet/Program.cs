bcb11b4a156d8258139cb83863db97e52051bdea
Lovejot Singh <lovejotsangha122@gmail.com>
2017-07-28 12:18:46 -0400

    Updated planet in planet class

--- abstract_Planet/Program.cs
+++ abstract_Planet/Program.cs
@@ -11,10 +11,13 @@ namespace abstract_Planet
         static void Main(string[] args)
         {
             Console.WriteLine("************************************ Planet Description ***********************************");
-            GiantPlanet giantPlanet = new GiantPlanet("Mars", 6779, 6.39, "GAS");
+            GiantPlanet giantPlanet = new GiantPlanet("Mercury", 4879, 3.285, "GAS");
+            GiantPlanet giantPlanet1 = new GiantPlanet("Jupitor", 139882, 317.8, "GAS");
+            GiantPlanet giantPlanet3 = new GiantPlanet("Mars", 6779, 6.39, "GAS");
             TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Earth", 12742, 5.9722, true);
             giantPlanet.ToString();
             terrestrialPlanet.ToString();
+            giantPlanet1.ToString();
             Program.WAITFORANYKEY();
         }
         static void WAITFORANYKEY()
