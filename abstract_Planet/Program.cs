0a316ad571af2c796ada0dd66a25e09449ed668f
Lovejot Singh <lovejotsangha122@gmail.com>
2017-07-28 12:48:53 -0400

    Modified program class

--- abstract_Planet/Program.cs
+++ abstract_Planet/Program.cs
@@ -12,12 +12,13 @@ namespace abstract_Planet
         {
             Console.WriteLine("************************************ Planet Description ***********************************");
             GiantPlanet giantPlanet = new GiantPlanet("Mercury", 4879, 3.285, "GAS");
-            GiantPlanet giantPlanet1 = new GiantPlanet("Jupitor", 139882, 317.8, "GAS");
-            GiantPlanet giantPlanet3 = new GiantPlanet("Mars", 6779, 6.39, "GAS");
             TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Earth", 12742, 5.9722, true);
+            GiantPlanet giantPlanet1 = new GiantPlanet("Venus", 12104, 4.867, "GAS");
+            GiantPlanet giantPlanet2 = new GiantPlanet("Mars", 6779, 6.39, "GAS");
             giantPlanet.ToString();
-            terrestrialPlanet.ToString();
             giantPlanet1.ToString();
+            terrestrialPlanet.ToString();
+            giantPlanet2.ToString();
             Program.WAITFORANYKEY();
         }
         static void WAITFORANYKEY()
